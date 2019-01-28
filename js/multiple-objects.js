8a0b194237ed2ec9f0216a6024971789dea7aa16
Gabriel Caiana Guedes <gabrielcaianaguedes@gmail.com>
2019-01-27 23:16:45 -0200

    obtendo id e exibindo na tela

--- js/multiple-objects.js
+++ js/multiple-objects.js
@@ -1,15 +1,23 @@
-// Criando objeto com notação de construtor, modelo para hoteis
-function Hotel(nome,quartos,reservas) {
-  this.nome = nome;
-  this.quartos = quartos;
-  this. reservados = reservas;
-  this.checarReservas = function() {
-    return this.quartos - this.reservas;
+  // Criando objeto com notação de construtor, modelo para hoteis
+  function Hotel(nome,quartos,reservas) {
+    this.nome = nome;
+    this.quartos = quartos;
+    this. reservados = reservas;
+    this.checarReservas = function() {
+      return this.quartos - this.reservados;
+    };
   }
-}
-//Criando instâncias de Objetos
-let caianaHotel = new Hotel('Caiana', 50, 25);
-let almeidaHotel = new Hotel('Almeida', 100, 30);
+  //Criando instâncias de Objetos
+  let caianaHotel = new Hotel('Caiana', 50, 25);
+  let almeidaHotel = new Hotel('Almeida', 100, 30);
 
-console.log(caianaHotel)
-console.log(almeidaHotel)
+  //Obtendo id e exibindo na página para o usuário
+  let detalhesHotelCaiana = caianaHotel.nome + ' quartos: ';
+      detalhesHotelCaiana += caianaHotel.checarReservas();
+  let elHotel = document.getElementById('hotel1');
+  elHotel.textContent = detalhesHotelCaiana;
+
+  let detalhesHotelAlmeida = almeidaHotel.nome + ' quartos: ';
+      detalhesHotelAlmeida += almeidaHotel.checarReservas();
+  let elHotel2 = document.getElementById('hotel2');
+  elHotel2.textContent = detalhesHotelAlmeida;
